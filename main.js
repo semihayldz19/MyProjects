dd0e5a1b8ada36927e0666ff3cc99a1624275b22
semiha yıldız karaderili <semihakaraderili@gmail.com>
2025-09-08 14:31:50 +0300

    yazılarım kısmı eklendi

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const titleElement1 = document.getElementById("title");
-const metin = titleElement1.textContent;
-titleElement1.textContent="";
-titleElement1.style.visibility = "visible"; // harf harf yazarken göster
-let index = 0;
+const titleElement1 = document.getElementById("title"); // id si title olan elementi alır ve titleElementt1 de saklar
+const metin = titleElement1.textContent; //titleElement1 deki metni alır ve metin e kaydeder
+titleElement1.textContent=""; // ekranda başlık birdenbire gözükmesin diye içini temizledik
+titleElement1.style.visibility = "visible"; // başlık elementinin görünürlüğünü visible(görünür) yaptık.
+let index = 0; // metnin içinde dolaşan sayaç
 
 function yazdir()
 {
-    if(index < metin.length) {
-        titleElement1.textContent += metin.charAt(index);
-        index ++;
-        setTimeout(yazdir,100); //her harf için 100 ms bekle
+    if(index < metin.length) { // index değişkeninin numarası metin boyutundan küçük olduğu sürece
+        titleElement1.textContent += metin.charAt(index);//o indexde olan karakteri textcontent e ekle
+        index ++; // indexi bir artır ki sonraki karaktere geçsin
+        setTimeout(yazdir,150); //150 milisaniye sonra fonksiyonu tekrar çağırır
     }
 }
-yazdir();
+yazdir(); // tanımladığımız fonksiyonu çağırdık.
 
